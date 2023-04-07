c6ec845bb6fbfcaaacfade81adafe132c5956a55
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-07 20:25:30 +0100

    request setup and configuration.

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,8 +3,11 @@ const {
 	collection,
 	addDoc,
 	getFirestore,
-	Firestore,
+	getDoc,
 	getDocs,
+	doc,
+	updateDoc,
+	deleteDoc,
 } = require("firebase/firestore");
 const firebaseApp = require("../db");
 const Student = require("../models/student");
@@ -51,7 +54,61 @@ const getAllStudents = async (req, res, next) => {
 	}
 };
 
+const getStudent = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const docRef = doc(db, "students", id);
+		const docSnap = await getDoc(docRef);
+		if (docSnap.exists()) {
+			const student = new Student(
+				docSnap.id,
+				docSnap.data().firstName,
+				docSnap.data().lastName,
+				docSnap.data().fatherName,
+				docSnap.data().classEnrolled,
+				docSnap.data().age,
+				docSnap.data().phoneNumber,
+				docSnap.data().subject,
+				docSnap.data().year,
+				docSnap.data().semester,
+				docSnap.data().status
+			);
+			res.status(200).send(student);
+		} else {
+			res.status(404).send("No student record found");
+		}
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
+
+const updateStudent = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const data = req.body;
+		const docRef = doc(db, "students", id);
+		await updateDoc(docRef, data);
+		res.status(200).send("Student record updated successfully");
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
+
+const deleteStudent = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const docRef = doc(db, "students", id);
+		await deleteDoc(docRef);
+		res.status(200).send("Student record deleted successfully");
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
+
 module.exports = {
 	addStudent,
 	getAllStudents,
+	getStudent,
+	updateStudent,
+	deleteStudent,
 };
--- routes/student-routes.js
+++ routes/student-routes.js
@@ -2,11 +2,18 @@ const express = require("express");
 const {
 	addStudent,
 	getAllStudents,
+	getStudent,
+	updateStudent,
+	deleteStudent,
 } = require("../controllers/studentController");
 
 const router = express.Router();
 
 router.post("/student", addStudent);
 router.get("/students", getAllStudents);
+router
+	.get("/student/:id", getStudent)
+	.put("/student/:id", updateStudent)
+	.delete("/student/:id", deleteStudent);
 
 module.exports = router;
